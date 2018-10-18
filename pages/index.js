cf68ccc249478ae8f428e0e3251088afbe5be5e7
Ian Baker <admin@ianbaker.me>
2018-10-18 21:33:00 +0100

    Basic example using Shared Components

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-export default() => <div><h1>Next branch</h1><img src="/static/me.jpg" alt="my image" /></div>
+import Layout from '../components/Layout'
+
+export default() => (
+    <div>
+        <Layout>
+            <h1>Ian Baker</h1><img src="/static/me.jpg" alt="my image" />
+        </Layout>
+    </div>
+)

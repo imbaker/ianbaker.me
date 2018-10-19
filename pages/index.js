bdc9b8aebf3e2e127d1078eb1afb244331a49091
Ian Baker <1401696+imbaker@users.noreply.github.com>
2018-10-19 13:31:49 +0100

    Create dynamic pages lesson completed

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Layout from '../components/Layout'
+import Link from 'next/link'
+
+const PostLink = (props) => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
 
 export default() => (
     <div>
         <Layout>
             <h1>Ian Baker</h1><img src="/static/me.jpg" alt="my image" />
+            <ul>
+                <PostLink title="Hello Next.js" />
+                <PostLink title="Some other title" />
+            </ul>
         </Layout>
     </div>
 )

c9b304fb35eaf752e6393bc4156454d1f077d198
Ian Baker <admin@ianbaker.me>
2018-11-11 15:22:54 +0000

    Added hidden form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout'
 
 export default () => (
     <form name="contact" method="POST" action="/success" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>Your Name: <input type="text" name="name" /></label>
       </p>

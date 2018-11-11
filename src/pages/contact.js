d972d0bfacd87b4571759091e9bb830192a47a09
Ian Baker <admin@ianbaker.me>
2018-11-11 14:59:32 +0000

    Test contact forms

---
+++ src/pages/contact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout subTitle='Contact'>
+    <form name="contact" method="POST" netlify>
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </Layout>
+)

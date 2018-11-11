898770824610897091225f9b6a2711b412bb2e4a
Ian Baker <admin@ianbaker.me>
2018-11-11 14:00:11 +0000

    Added initial Prismic integration

--- src/pages/springsteen.js
+++ src/pages/springsteen.js
@@ -11,6 +11,15 @@ const Concert = props => (
   </li>
 )
 
+const Songs = props => (
+  <ul className={styles.songs}>
+    <li key={ props.songs[0].key }>{ props.songs[0].song_title.text }</li>
+    <li key={ props.songs[1].key }>{ props.songs[1].song_title.text }</li>
+    <li key={ props.songs[2].key }>{ props.songs[2].song_title.text }</li>
+    <li key={ props.songs[3].key }>{ props.songs[3].song_title.text }</li>
+  </ul>
+)
+
 export default ({ data }) => (
   <Layout subTitle='Bruce Springsteen Concerts'>
     <div className={styles.container}>
@@ -22,7 +31,23 @@ export default ({ data }) => (
             <div className={styles.col3}>Location</div>
         </li>
         {data.allDataJson.edges.map(({ node }, index) => (
-          <Concert key = { node.id } index = { index + 1 } date = {node.date} location = {node.location} />
+            <Concert key = { node.id } index = { index + 1 } date = {node.date} location = {node.location} />
+        ))}
+      </ul>
+    </div>
+    <div className={styles.container}>
+      <h3>{data.allPrismicConcerts.totalCount} concerts</h3>
+      <ul className={styles.responsivetable}>
+        <li className={styles.tableheader}>
+            <div className={styles.col1}>#</div>
+            <div className={styles.col2}>Date</div>
+            <div className={styles.col3}>Location</div>
+        </li>
+        {data.allPrismicConcerts.edges.map(({ node }, index) => (
+          <div key={ node.id }>
+            <Concert key = { node.id } index = { index + 1 } date = {node.data.date} location = {node.data.location.text} />
+            <Songs key = { node.id } songs = { node.data.songs } />
+          </div>
         ))}
       </ul>
     </div>
@@ -37,7 +62,27 @@ export const query = graphql`
         node {
           id
           location
-          date(formatString: "ddd DD MMMM YYYY")
+          date(formatString: "ddd D MMMM YYYY")
+        }
+      }
+    }
+    allPrismicConcerts(sort: { order: ASC, fields: [data___date]  }) {
+      totalCount
+      edges {
+        node {
+          data {
+            location {
+              html
+              text
+            }
+            date(formatString: "ddd D MMMM YYYY")
+            songs {
+              song_title {
+                html
+                text
+              }
+            }
+          }
         }
       }
     }

d6f2da7fc53a4d551eaa17b49333363058129139
Ian Baker <inbox@ianbaker.me>
2024-11-26 21:41:10 +0000

    Initial "empty" Astro version

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx:stable
-
-COPY ./public/ /data/www/
-COPY ./nginx.conf /etc/nginx/nginx.conf
-
-CMD ["nginx", "-c", "/etc/nginx/nginx.conf", "-g", "daemon off;"]
--- src/utils/typography.js
+++
@@ -1,6 +0,0 @@
-import Typography from "typography"
-import Theme from "typography-theme-st-annes"
-
-// const typography = new Typography(Theme)
-
-// export default typography

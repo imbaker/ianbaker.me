350357ccd0b825d84ac396f44ccd0ad802637041
Ian Baker <inbox@ianbaker.me>
2026-07-22 21:11:30 +0100

    chore: Fixed lint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,23 @@
+import { defineConfig } from 'eslint/config';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import tailwindcss from 'eslint-plugin-tailwindcss';
 
-export default [
+export default defineConfig([
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
     plugins: { tailwindcss },
+    settings: {
+      tailwindcss: {
+        cssConfigPath: './src/styles/global.css',
+      },
+    },
     rules: {
       'tailwindcss/classnames-order': 'error',
-    }
-  }
-];
+    },
+  },
+]);

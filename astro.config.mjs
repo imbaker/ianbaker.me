33d88ae090beb772569a9fb920ed3954bb63d6cb
Ian Baker <inbox@ianbaker.me>
2026-07-28 22:10:01 +0100

    chore: fix Prettier configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,29 @@
-import { defineConfig, fontProviders } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://ianbaker.me',
+  site: "https://ianbaker.me",
 
-  output: 'static',
+  output: "static",
 
   integrations: [
     sitemap({
-      filter: (page) => page !== 'https://ianbaker.me/alive/' // Exclude healthcheck page from sitemap
-    })],
+      filter: (page) => page !== "https://ianbaker.me/alive/", // Exclude healthcheck page from sitemap
+    }),
+  ],
 
   fonts: [
-      {
-        name: 'Lato',
-        provider: fontProviders.google(),
-        weights: ['400', '700'],
-        cssVariable: '--font-lato',
-        display: 'swap',
-      }],
+    {
+      name: "Lato",
+      provider: fontProviders.google(),
+      weights: ["400", "700"],
+      cssVariable: "--font-lato",
+      display: "swap",
+    },
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });

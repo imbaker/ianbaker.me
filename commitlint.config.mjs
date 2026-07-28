6d0e6e5f0cd5f961eefb3f0d9f2788362cb49faa
Ian Baker <inbox@ianbaker.me>
2026-07-28 21:47:18 +0100

    chore: enforce conventional commits via commitlint and Husky hook
    See https://www.conventionalcommits.org/ for the full specification

---
+++ commitlint.config.mjs
@@ -0,0 +1,32 @@
+import { default as conventional } from '@commitlint/config-conventional';
+
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'security',
+        'deps'
+      ]
+    ],
+    'subject-case': [2, 'never', ['pascal-case']],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', ['lower-case']],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never']
+  }
+};
